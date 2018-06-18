30bc25851e610f6a7a1091ded3eabc40d4659960
LooorTor <LooorTor@gmail.com>
2018-06-18 09:53:22 +0800

    feat: add worldcup symbol info

---
+++ app/src/main/java/com/wagerr/wallet/data/bet/EventSymbol.kt
@@ -0,0 +1,112 @@
+package com.wagerr.wallet.data.bet
+
+import android.support.annotation.DrawableRes
+import com.wagerr.wallet.R
+
+sealed class EventSymbol {
+    abstract fun getFullEventInfo(eventInfoSymbol: String): String
+    abstract fun getFullEventLeague(): String
+    abstract fun getFullTeam(teamSymbol: String): String
+    abstract fun getTeamImage(teamSymbol: String): Int?
+
+
+    object WorldCupSymbol : EventSymbol() {
+
+        val TEAM_MAP = hashMapOf(
+                "ARG" to "Argentina",
+                "AUS" to "Australia",
+                "BRA" to "Brazil",
+                "BEL" to "Belgium",
+                "CRO" to "Croatia",
+                "COL" to "Colombia",
+                "CRC" to "Costa Rica",
+                "DEN" to "Denmark",
+                "EGY" to "Egypt",
+                "ENG" to "England",
+                "ESP" to "Spain",
+                "FRA" to "France",
+                "GER" to "Germany",
+                "IRN" to "IR Iran",
+                "ISL" to "Iceland",
+                "JPN" to "Japan",
+                "KOR" to "Korea Republic",
+                "KSA" to "Saudi Arabia",
+                "MAR" to "Morocco",
+                "MEX" to "Mexico",
+                "NIG" to "Nigeria",
+                "PAN" to "Panama",
+                "RER" to "Peru",
+                "POR" to "Portugal",
+                "POL" to "Poland",
+                "RUS" to "Russia",
+                "SRB" to "Serbia",
+                "SEN" to "Senegal",
+                "SUI" to "Switzerland",
+                "SWE" to "Sweden",
+                "TUN" to "Tunisia",
+                "URU" to "Uruguay"
+        )
+
+        val TEAM_IMAGE_MAP = hashMapOf(
+                "ARG" to R.drawable.img_flag_arg,
+                "AUS" to R.drawable.img_flag_aus,
+                "BRA" to R.drawable.img_flag_bra,
+                "BEL" to R.drawable.img_flag_bel,
+                "CRO" to R.drawable.img_flag_cro,
+                "COL" to R.drawable.img_flag_col,
+                "CRC" to R.drawable.img_flag_crc,
+                "DEN" to R.drawable.img_flag_den,
+                "EGY" to R.drawable.img_flag_egy,
+                "ENG" to R.drawable.img_flag_eng,
+                "ESP" to R.drawable.img_flag_esp,
+                "FRA" to R.drawable.img_flag_fra,
+                "GER" to R.drawable.img_flag_ger,
+                "IRN" to R.drawable.img_flag_irn,
+                "ISL" to R.drawable.img_flag_isl,
+                "JPN" to R.drawable.img_flag_jpn,
+                "KOR" to R.drawable.img_flag_kor,
+                "KSA" to R.drawable.img_flag_ksa,
+                "MAR" to R.drawable.img_flag_mar,
+                "MEX" to R.drawable.img_flag_mex,
+                "NIG" to R.drawable.img_flag_nig,
+                "PAN" to R.drawable.img_flag_pan,
+                "RER" to R.drawable.img_flag_per,
+                "POR" to R.drawable.img_flag_por,
+                "POL" to R.drawable.img_flag_pol,
+                "RUS" to R.drawable.img_flag_rus,
+                "SRB" to R.drawable.img_flag_srb,
+                "SEN" to R.drawable.img_flag_sen,
+                "SUI" to R.drawable.img_flag_sui,
+                "SWE" to R.drawable.img_flag_swe,
+                "TUN" to R.drawable.img_flag_tun,
+                "URU" to R.drawable.img_flag_uru
+        )
+        val EVENT_INFO_MAP = hashMapOf(
+                "R1" to "Round 1"
+        )
+
+        override fun getFullEventInfo(eventInfoSymbol: String): String {
+            return EVENT_INFO_MAP[eventInfoSymbol] ?: ""
+        }
+
+        override fun getFullEventLeague(): String {
+            return "World Cup"
+        }
+
+        override fun getFullTeam(teamSymbol: String): String {
+            return TEAM_MAP[teamSymbol] ?: ""
+        }
+
+        override fun getTeamImage(teamSymbol: String): Int? {
+            return TEAM_IMAGE_MAP[teamSymbol]
+        }
+
+    }
+}
+
+fun String.toEventSymbol(): EventSymbol {
+    return when (this) {
+        "WCUP" -> EventSymbol.WorldCupSymbol
+        else -> EventSymbol.WorldCupSymbol
+    }
+}
--- app/src/main/java/com/wagerr/wallet/ui/bet/event/BetEventAdapter.kt
+++ app/src/main/java/com/wagerr/wallet/ui/bet/event/BetEventAdapter.kt
@@ -4,9 +4,7 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.wagerr.wallet.R
 import com.wagerr.wallet.data.bet.BetEvent
-import org.apache.commons.codec.binary.Hex
-import org.wagerrj.core.Transaction
-import java.nio.charset.Charset
+import com.wagerr.wallet.data.bet.toEventSymbol
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,12 +12,19 @@ class BetEventAdapter : BaseQuickAdapter<BetEvent, BaseViewHolder>(R.layout.item
 
     override fun convert(helper: BaseViewHolder, item: BetEvent) {
 
+        val eventSymbol = item.eventLeague.toEventSymbol()
         helper.setText(R.id.text_event_id, item.eventId)
-        helper.setText(R.id.text_event_league, item.eventLeague)
-        helper.setText(R.id.text_event_info, item.eventInfo)
+        helper.setText(R.id.text_event_league,eventSymbol.getFullEventLeague())
+        helper.setText(R.id.text_event_info, eventSymbol.getFullEventInfo(item.eventInfo))
         helper.setText(R.id.text_time, Date(item.timeStamp).formatToViewDateTimeDefaults())
-        helper.setText(R.id.text_home_team, item.homeTeam)
-        helper.setText(R.id.text_away_team, item.awayTeam)
+        helper.setText(R.id.text_home_team, eventSymbol.getFullTeam(item.homeTeam))
+        helper.setText(R.id.text_away_team, eventSymbol.getFullTeam(item.awayTeam))
+        eventSymbol.getTeamImage(item.homeTeam)?.let {
+            helper.setImageResource(R.id.image_home_team, it)
+        }
+        eventSymbol.getTeamImage(item.awayTeam)?.let {
+            helper.setImageResource(R.id.image_away_team, it)
+        }
         helper.setText(R.id.button_home_odds, "${item.homeTeam} WIN\n${item.homeOdds}")
         helper.setText(R.id.button_draw_odds, "DRAW\n${item.drawOdds}")
         helper.setText(R.id.button_away_odds, "${item.awayTeam} WIN\n${item.awayOdds}")
