20d46220c1ace07a398cdb030629cacee2a589f6
LooorTor <LooorTor@gmail.com>
2018-07-12 22:29:54 +0800

    feat: add tx detail choose for winning bet in event detail

--- app/src/main/java/com/wagerr/wallet/ui/bet/result/BetEventDetailActivity.kt
+++ app/src/main/java/com/wagerr/wallet/ui/bet/result/BetEventDetailActivity.kt
@@ -5,9 +5,13 @@ import android.content.Intent
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.wagerr.wallet.R
 import com.wagerr.wallet.WagerrApplication
 import com.wagerr.wallet.data.bet.getMatchResult
@@ -15,21 +19,21 @@ import com.wagerr.wallet.data.bet.toEventSymbol
 import com.wagerr.wallet.data.worldcup.api.WorldCupApi
 import com.wagerr.wallet.module.bet.BetEventFetcher
 import com.wagerr.wallet.ui.base.BaseActivity
+import com.wagerr.wallet.ui.bet.event.BetEventActivity
 import com.wagerr.wallet.ui.transaction_detail_activity.FragmentTxDetail
 import com.wagerr.wallet.ui.transaction_detail_activity.TransactionDetailActivity
 import com.wagerr.wallet.ui.transaction_detail_activity.TransactionIdDetailActivity
 import com.wagerr.wallet.ui.transaction_detail_activity.TransactionIdDetailFragment
 import com.wagerr.wallet.utils.formatToViewDateTimeDefaults
+import com.wagerr.wallet.utils.wrapContent
+import global.WagerrCoreContext
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_finished_bet_event_detail.*
 import org.wagerrj.core.Sha256Hash
-import wagerr.bet.DRAW_SYMBOL
-import wagerr.bet.getBetEventsById
-import wagerr.bet.getBetResultByEventId
-import wagerr.bet.toBetActions
+import wagerr.bet.*
 import java.util.Date
 
 
@@ -65,10 +69,38 @@ class BetEventDetailActivity : BaseActivity() {
         bet_action_list.adapter = betActionsAdapter
         betActionsAdapter.bindToRecyclerView(bet_action_list)
         betActionsAdapter.setOnItemClickListener { adapter, view, position ->
-            val betAction = betActionsAdapter.getItem(position)!!.betAction!!
-            
+            val betActionData = betActionsAdapter.getItem(position)!!
+            if (betActionData.betReward != null) {
+                showSelectDialog(betActionData)
+            } else {
+                Observable.fromCallable {
+                    WagerrApplication.getInstance().module.getTxWrapper(betActionData.betAction.transaction.hashAsString)
+                }.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({
+                            val bundle = Bundle()
+                            bundle.putSerializable(TransactionIdDetailFragment.EXTRA_TX_ID, it.transaction.hashAsString)
+                            bundle.putBoolean(TransactionIdDetailFragment.IS_DETAIL, true)
+                            val intent = Intent(this, TransactionIdDetailActivity::class.java)
+                            intent.putExtras(bundle)
+                            startActivity(intent)
+                        }, {})
+            }
+        }
+
+        load()
+    }
+
+    private fun showSelectDialog(betActionData:BetData.BetActionData) {
+        val sheetView = layoutInflater.inflate(R.layout.dialog_choose_action_reward, null)
+        val seeActionTx = sheetView.findViewById<TextView>(R.id.text_see_action_tx)
+        val seeRewardTx = sheetView.findViewById<TextView>(R.id.text_see_reward_tx)
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setView(sheetView)
+        val dialog = dialogBuilder.create()
+        seeActionTx.setOnClickListener {
             Observable.fromCallable {
-                WagerrApplication.getInstance().module.getTxWrapper(betAction.transaction.hashAsString)
+                WagerrApplication.getInstance().module.getTxWrapper(betActionData.betAction.transaction.hashAsString)
             }.subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe({
@@ -78,10 +110,26 @@ class BetEventDetailActivity : BaseActivity() {
                         val intent = Intent(this, TransactionIdDetailActivity::class.java)
                         intent.putExtras(bundle)
                         startActivity(intent)
+                        dialog.dismiss()
                     }, {})
         }
-
-        load()
+        seeRewardTx.setOnClickListener {
+            Observable.fromCallable {
+                WagerrApplication.getInstance().module.getTxWrapper(betActionData.betReward!!.transaction.hashAsString)
+            }.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        val bundle = Bundle()
+                        bundle.putSerializable(TransactionIdDetailFragment.EXTRA_TX_ID, it.transaction.hashAsString)
+                        bundle.putBoolean(TransactionIdDetailFragment.IS_DETAIL, true)
+                        val intent = Intent(this, TransactionIdDetailActivity::class.java)
+                        intent.putExtras(bundle)
+                        startActivity(intent)
+                        dialog.dismiss()
+                    }, {})
+        }
+        dialog.show()
+        dialog.wrapContent()
     }
 
     private fun load() {
