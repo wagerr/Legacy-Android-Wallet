5ceec0562d00fc70ff9640fe9b31590cd9223c10
LooorTor <LooorTor@gmail.com>
2018-09-13 06:53:06 +0800

    feat: update mainnet nodes

--- wagerr-light-wallets-core/src/main/java/network/PeerGlobalData.java
+++ wagerr-light-wallets-core/src/main/java/network/PeerGlobalData.java
@@ -12,7 +12,7 @@ public class PeerGlobalData {
     public static final String[] TRUSTED_TEST_NODES = new String[]{"35.237.41.190", "35.227.30.42",
             "35.237.175.155", "35.196.21.183"};
 
-    public static final String[] TRUSTED_NODES = new String[]{"46.166.148.3"};
+    public static final String[] TRUSTED_NODES = new String[]{"sg.wagerr.com", "jp.wagerr.com", "nl.wagerr.com", "au.wagerr.com", "ny.wagerr.com", "la.wagerr.com", "la.wagerr.com"};
 
     public static final List<PeerData> listTrustedHosts() {
         List<PeerData> list = new ArrayList<>();
